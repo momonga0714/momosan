c58c51abda2097f27f38a66106954c2136a913e3
tomokitakenaga <tomoki0902@gmail.com>
2020-04-17 22:16:40 +0900

    first push

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,44 @@
+class GroupsController < ApplicationController
+  before_action :authenticate_user!, except: [:index,:create]
+  before_action :set_message
+  
+  def index
+    @groups = Group.includes(:user)
+    @group = Group.new
+  end
+
+  def new
+    @group = Group.new
+    @group.users << current_user
+  end
+
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: "グループ作成しました"
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to group_messages_path(params[:id]), notice: "グループを編集しました"
+    else
+      render :edit
+    end
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name, user_ids: [])
+  end
+  def set_message
+    
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root "groups#index"
+  resources :users, only:[:index,:edit,:update,:destroy]
+  resources :groups, only:[:index, :new,:create,:edit,:update] do
+    resources :messages, only:[:index, :new,:create]
+    namespace :api do
+      resources :messages, only: :index, defaults: { format: 'json' }
+    end
+  end
+end
