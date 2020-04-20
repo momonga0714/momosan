637a0d47e3435dc9a2dba476d407f3601fa0ae82
tomokitakenaga <tomoki0902@gmail.com>
2020-04-20 14:27:43 +0900

    レスポンスデザイン調整

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root "groups#index"
   resources :users, only:[:index,:edit,:update,:destroy]
-  resources :groups, only:[:index, :new,:create,:edit,:update] do
-    resources :messages, only:[:index, :new,:create]
+  resources :groups, only:[:index, :new,:create,:edit,:update,:destroy] do
+    resources :messages, only:[:index, :new,:create,:destroy]
     namespace :api do
       resources :messages, only: :index, defaults: { format: 'json' }
     end
